d6ab2c5556cb02363ee79fba8b48d80be4f1c851
vagnermengali <vagmengali2015@gmail.com>
2024-11-30 14:48:37 -0300

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: false,
-      },
-      base: "/livros-vai-na-web"
-    },
-  },
-});
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+   },
+   base: "/livros-vai-na-web"
+})
